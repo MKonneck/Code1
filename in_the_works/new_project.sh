670e8106972c5d842d4944d8fcf314efa8a07e34
MKonneck <konneck.matthew@gmail.com>
2025-11-05 18:40:43 -0500

    .

---
+++ in_the_works/new_project.sh
@@ -0,0 +1,17 @@
+# -------------------------------------------------------------------------------
+#
+#  PROJECT: The Interactive Code Runner
+#
+#  @AUTHOR: Matthew Konneck
+#  @VERSION: 1.0
+#  @DATE: 2025/11/05
+#
+#  DESCRIPTION: This script provides a command-line menu to the user,
+#               allowing them to select and execute various common coding tasks
+#               and system utilities using IF and GOTO commands for control flow.
+#
+# -------------------------------------------------------------------------------
+
+#!/bin/bash
+
+read -p "Name: " project_name
