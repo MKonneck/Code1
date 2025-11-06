10917f183dac278782d6b69c3cb46fb816c6eec0
Matthew Konneck <108560245+Makoe30@users.noreply.github.com>
2025-11-05 22:36:50 -0500

    .

---
+++ in_the_works/automated_backup.bat
@@ -0,0 +1,16 @@
+REM -------------------------------------------------------------------------------
+REM
+REM  PROJECT: A script that backs up a specific folder and uses a dedicated
+REM           function to display status messages
+REM
+REM  @AUTHOR: Matthew Konneck
+REM  @VERSION: 1.0
+REM  @DATE: 2025/11/05
+REM
+REM  DESCRIPTION: This script prompts the user for a specific file to organize
+REM               
+REM               
+REM
+REM -------------------------------------------------------------------------------
+
+echo off
