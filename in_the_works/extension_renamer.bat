409e143f0e8fa00f6bfe34e97c43bd8bf44559fa
Matthew Konneck <108560245+Makoe30@users.noreply.github.com>
2025-11-06 00:34:23 -0500

    .

---
+++ in_the_works/extension_renamer.bat
@@ -0,0 +1,12 @@
+REM -------------------------------------------------------------------------------
+REM
+REM  PROJECT: The file extension renamer
+REM
+REM  @AUTHOR: Matthew Konneck
+REM  @VERSION: 1.0
+REM  @DATE: 2025/11/06
+REM
+REM  DESCRIPTION: This script checks if a command worked correctly and uses the 
+REM               for command to rename files in bulk.
+REM               
+REM -------------------------------------------------------------------------------
