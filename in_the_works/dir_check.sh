838ae159737822c60ed96c874f45fd58e74ca949
MKonneck <konneck.matthew@gmail.com>
2025-11-11 14:06:40 -0500

    finished

---
+++ in_the_works/dir_check.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------------
+#
+#  PROJECT: Asks the user for a file path and checks if file path exists
+#
+#  @AUTHOR: Matthew Konneck
+#  @VERSION: 1.0
+#  @DATE: 2025/11/11
+#
+#  DESCRIPTION: This project asks the user for a full file path of a folder and
+#               checks the computer to make sure the file is actually on the 
+#               computer.
+#
+# -------------------------------------------------------------------------------
+
+read -p "Enter folder path: " folder_path
+
+if [ -d "$folder_path" ]; 
+then echo "Congradulations this folder exists!!";
+else echo "Oopsie daisy, this folder doesn't exist ):"
+fi
