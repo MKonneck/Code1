543c93ef05eac8d5194a92ddbfd249cd9ff402c9
MKonneck <konneck.matthew@gmail.com>
2025-11-11 11:57:42 -0500

    .

---
+++ in_the_works/greeter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------------
+#
+#  PROJECT: Prints out a message of choosing to the terminal
+#
+#  @AUTHOR: Matthew Konneck
+#  @VERSION: 1.0
+#  @DATE: 2025/11/11
+#
+#  DESCRIPTION: This script prints out a message of choosing to the terminal
+#               using a set variable decided by the user
+#               
+#
+# -------------------------------------------------------------------------------
+
