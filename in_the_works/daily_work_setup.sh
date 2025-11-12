ef072c42640a942b8978180e17f20a7639159b02
MKonneck <konneck.matthew@gmail.com>
2025-11-12 11:33:30 -0500

    .

--- in_the_works/daily_work_setup.sh
+++ in_the_works/daily_work_setup.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+
+#---------------------------------------------------------------------
+#
+#
+#  PROJECT: Daily work setup
+#
+#  @AUTHOR: Matthew Konneck
+#  @VERSION: V1.0
+#  @DATE: 11-12-25
+#
+#  DESCRIPTION: Create a script that, when run, will check for and 
+#               create a new directory named with today's date 
+#               in a specific format (e.g., YYYY-MM-DD_Daily_Work).
+#                            
+#
+#
+#---------------------------------------------------------------------
+
 TODAY=$(date +"%Y-%m-%d")
 mkdir "${TODAY}_Daily_Work"
