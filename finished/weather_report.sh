f9ecacf1f4013106d6498a59e8aff011333d8945
MKonneck <konneck.matthew@gmail.com>
2025-11-13 15:18:46 -0500

    weather_report.sh Finished and portable

--- finished/weather_report.sh
+++ finished/weather_report.sh
@@ -13,20 +13,31 @@
 #               forecast for your location and display a simple 
 #               summary using an osascript notification.
 #
+#  -------------------------------------------------------------------
+#  USER CONFIGURATION BLOCK
+#
+#  @param DEFAULT_CITY: The directory to archive by default.
+#  @param WEATHER_FORMAT: The destination for the archived files.  
+#
+#  -------------------------------------------------------------------
+#
 #---------------------------------------------------------------------
 
-CITY_NAME=$(osascript -e 'display dialog "Input your current City:" default answer "Holland_mi"' -e 'text returned of result')
+# --- CONFIGURATION VARIABLES ---
+DEFAULT_CITY="Holland_MI"
+WEATHER_FORMAT="?format=%c%C\n%t\n%p\n%w"
+# -------------------------------
+
+CITY_NAME=$(osascript -e 'display dialog "Input your current City:" default answer "'$DEFAULT_CITY'"' -e 'text returned of result')
 CITY_NAME=$(echo "$CITY_NAME" | sed 's/ /_/g')
-WEATHER_URL="wttr.in/$CITY_NAME?format=3"
+WEATHER_URL="wttr.in/$CITY_NAME$WEATHER_FORMAT"
 
 RAW_WEATHER_DATA=$(curl -s "$WEATHER_URL")
-
-CLEAN_WEATHER_DATA=$(echo "$RAW_WEATHER_DATA" | tr -d '[:cntrl:]' | sed 's/"/\\"/g')
 
 DISPLAY_CITY_NAME=$(echo "$CITY_NAME" | tr '_' ' ')
 
 NOTIFICATION_TITLE="Current Weather for $DISPLAY_CITY_NAME"
 
-osascript -e "display alert \"Current Weather for $DISPLAY_CITY_NAME\" message \"$CLEAN_WEATHER_DATA\""
+osascript -e "display alert \"Current Weather for $DISPLAY_CITY_NAME\" message \"$RAW_WEATHER_DATA\""
 
-echo "Weather Fetched: $CLEAN_WEATHER_DATA"
+echo "Weather Fetched: $RAW_WEATHER_DATA"
