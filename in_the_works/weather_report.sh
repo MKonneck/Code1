8c8084c3ce3bbf19811db32ae4339fa837d3782b
MKonneck <konneck.matthew@gmail.com>
2025-11-13 12:04:34 -0500

    .

---
+++ in_the_works/weather_report.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#---------------------------------------------------------------------
+#
+#
+#  PROJECT: Automated weather reporter
+#
+#  @AUTHOR: Matthew Konneck
+#  @VERSION: V1.0
+#  @DATE: 11-13-25
+#
+#  DESCRIPTION: Uses a command-line tool to fetch the current weather 
+#               forecast for your location and display a simple 
+#               summary using an osascript notification.
+#
+#---------------------------------------------------------------------
+
+WEATHER_DATA=
