bb637960ad1130c79b118989f8e3049742b2e4a1
MKonneck <konneck.matthew@gmail.com>
2025-11-13 14:58:43 -0500

    daily_backup.sh Finished and portable

--- finished/daily_backup.sh
+++ finished/daily_backup.sh
@@ -12,19 +12,36 @@
 #  DESCRIPTION: Creates a compressed .tar.gz archive of a source directory 
 #               and saves it to a user-specified destination.
 #
+#  -------------------------------------------------------------------
+#  USER CONFIGURATION BLOCK
+#
+#  @param DEFAULT_SOURCE_DIRECTORY: The directory to archive by default.
+#  @param DEFAULT_DESTINATION_DIRECTORY: The destination for the archived files.  
+#  @param FILE_PREFIX: The static text that begins the backup filename
+#                      (e.g., "archive" or "server_data").
+#
+#  -------------------------------------------------------------------
+#
 #---------------------------------------------------------------------
 
-SOURCE_DIR=$(osascript -e 'display dialog "Enter the FULL path of the directory to BACKUP:" default answer "/Users/'$USER'/Documents"' -e 'text returned of result')
+# --- CONFIGURATION VARIABLES ---
+DEFAULT_SOURCE_DIRECTORY="$HOME/Documents"
+DEFAULT_DESTINATION_DIRECTORY="$HOME/Backups"
+FILE_PREFIX="archive"
+# -------------------------------
+
+SOURCE_DIR=$(osascript -e 'display dialog "Enter the FULL path of the directory to BACKUP:" default answer "'$DEFAULT_SOURCE_DIRECTORY'"' -e 'text returned of result')
 
 if [ ! -d "$SOURCE_DIR" ]; then
     osascript -e "display alert \"Backup Failed!\" message \"Source directory not found: $SOURCE_DIR\" buttons {\"OK\"}"
     exit 1
 fi
 
-BACKUP_DIR=$(osascript -e 'display dialog "Enter the FULL path for the backup destination:" default answer "/Users/'$USER'/Backups"' -e 'text returned of result')
+BACKUP_DIR=$(osascript -e 'display dialog "Enter the FULL path for the backup destination:" default answer "'$DEFAULT_DESTINATION_DIRECTORY'"' -e 'text returned of result')
 
 DATE_SUFFIX=$(date +%Y%m%d_%H)
-FILENAME="backup_${DATE_SUFFIX}.tar.gz"
+SOURCE_BASENAME=$(basename "$SOURCE_DIR")
+FILENAME="${FILE_PREFIX}_${SOURCE_BASENAME}_${DATE_SUFFIX}.tar.gz"
 
 ARCHIVE_PATH="$BACKUP_DIR/$FILENAME"
 
